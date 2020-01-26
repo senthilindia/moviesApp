8d4fad088ac39a25c25c91ccae466e6f574c4419
senthilindia <senthilindia17@gmail.com>
2020-01-26 13:41:32 +0530

    Updates

---
+++ precache-manifest.a69d2fd0c91b977e5dadad92fa09d250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ddd070531d5a7deb0fd60feb217457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d32245c323d4b8e21c30",
+    "url": "/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "7f2e137c22b7f80c5390",
+    "url": "/static/js/2.3ebc8df1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3ebc8df1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d32245c323d4b8e21c30",
+    "url": "/static/js/main.226a3a31.chunk.js"
+  },
+  {
+    "revision": "cbd15f28e74071f050c0",
+    "url": "/static/js/runtime-main.19bbfaa4.js"
+  }
+]);
